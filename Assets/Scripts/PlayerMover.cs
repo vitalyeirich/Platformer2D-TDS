0fb0dd7d025f0395e258bfac9c6f4dfe6d4fa3b4
Vitaly Eirich <vitalyeirich@gmail.com>
2025-04-14 21:05:46 +0200

    Revert "Очистил файл"
    This reverts commit 841025f5e6262e6c90f9755449c32026bf4ad6c0.

--- Assets/Scripts/PlayerMover.cs
+++ Assets/Scripts/PlayerMover.cs
@@ -7,6 +7,7 @@ using UnityEngine.EventSystems;
 [RequireComponent(typeof(Rigidbody2D))]
 
 // Значение гравитации уже выставлено на 0 при старте
+// Тест юникод
 
 public class TopDownPlayer : MonoBehaviour
 {

71f63063275b8b8fd67230f76020370843ec8de8
Vitaly Eirich <vitalyeirich@gmail.com>
2025-04-14 22:03:40 +0200

    правильная скорость

--- Assets/Scripts/PlayerMover.cs
+++ Assets/Scripts/PlayerMover.cs
@@ -11,11 +11,8 @@ using UnityEngine.EventSystems;
 public class TopDownPlayer : MonoBehaviour
 {
     // обычная скорость
-<<<<<<< Updated upstream
-    public float moveSpeed = 10;
-=======
     public float moveSpeed = 10f;
->>>>>>> Stashed changes
+
 
     // скорость рывка
     public float dashSpeed = 15f;

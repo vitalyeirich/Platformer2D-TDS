4430d69258c06b7f6953c8033f53a8def437a9bb
Vitaly Eirich <vitalyeirich@gmail.com>
2025-04-14 21:51:47 +0200

    Update PlayerMover.cs

--- Assets/Scripts/PlayerMover.cs
+++ Assets/Scripts/PlayerMover.cs
@@ -11,7 +11,7 @@ using UnityEngine.EventSystems;
 public class TopDownPlayer : MonoBehaviour
 {
     // обычная скорость
-    public float moveSpeed = 5f;
+    public float moveSpeed = 50f;
 
     // скорость рывка
     public float dashSpeed = 15f;

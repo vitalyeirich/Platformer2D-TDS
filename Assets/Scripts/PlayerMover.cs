bfd169bbc27b5c94e7906d8efcb6f967bdf8c77a
Vitaly Eirich <vitalyeirich@gmail.com>
2025-04-14 20:35:19 +0200

    просто тест 2

--- Assets/Scripts/PlayerMover.cs
+++ Assets/Scripts/PlayerMover.cs
@@ -7,7 +7,7 @@ using UnityEngine.EventSystems;
 [RequireComponent(typeof(Rigidbody2D))]
 
 // Значение гравитации уже выставлено на 0 при старте
-// Поменял кодировку
+// Поменял кодировку, тест 2
 
 public class TopDownPlayer : MonoBehaviour
 {
